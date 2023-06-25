58b6fd81c7ee72adfd64281541e84a066f38468f
haguro <1076+haguro@users.noreply.github.com>
2023-06-25 15:59:04 +1000

    Add package, type and function comments

--- server/server.go
+++ server/server.go
@@ -1,3 +1,9 @@
+// Package server provides a HTTP server implementation for a Battlesnake game.
+//
+// This package defines the data structures used in the game. It also provides a
+// HTTP server that receives game events and responds with game moves.
+//
+// For more information about the Battlesnake game API, see https://docs.battlesnake.com/.
 package server
 
 import (
@@ -11,11 +17,13 @@ import (
 
 const apiVersion = "1"
 
+// Coord represents a coordinate on the game board.
 type Coord struct {
 	X int `json:"x"`
 	Y int `json:"y"`
 }
 
+// Battlesnake represents a Battlesnake on the game board.
 type Battlesnake struct {
 	ID             string         `json:"id"`
 	Name           string         `json:"name"`
@@ -29,12 +37,15 @@ type Battlesnake struct {
 	Customizations Customizations `json:"customizations"`
 }
 
+// Customizations represents the customizable attributes of a Battlesnake. Namely its color
+// as well as head and tail graphics.
 type Customizations struct {
 	Color string `json:"color"`
 	Head  string `json:"head"`
 	Tail  string `json:"tail"`
 }
 
+// Board represents the game board.
 type Board struct {
 	Height  int           `json:"height"`
 	Width   int           `json:"width"`
@@ -43,6 +54,9 @@ type Board struct {
 	Snakes  []Battlesnake `json:"snakes"`
 }
 
+// GameState represents state of the game at any given turn. It includes a Game instance, the
+// current turn, a board instance and the player's battlesnake.
+// It is passed with every 'move' request.
 type GameState struct {
 	Game  Game        `json:"game"`
 	Turn  int         `json:"turn"`
@@ -50,6 +64,7 @@ type GameState struct {
 	You   Battlesnake `json:"you"`
 }
 
+// Game represents a Battlesnake game.
 type Game struct {
 	ID      string  `json:"id"`
 	Ruleset Ruleset `json:"ruleset"`
@@ -58,26 +73,30 @@ type Game struct {
 	Timeout int     `json:"timeout"`
 }
 
+// Ruleset describes the rule set of a Battlesnake game.
 type Ruleset struct {
 	Name     string          `json:"name"`
 	Version  string          `json:"version"`
 	Settings RulesetSettings `json:"settings"`
 }
 
+// RulesetSettings describes all settings for a game rule set.
 type RulesetSettings struct {
-	FoodSpawnChance     int           `json:"foodSpawnChance"`
-	MinimumFood         int           `json:"minimumFood"`
-	HazardDamagePerTurn int           `json:"hazardDamagePerTurn"`
-	HazardMap           string        `json:"hazardMap"`
-	HazardMapAuthor     string        `json:"hazardMapAuthor"`
-	Royale              RoyalSettings `json:"royale"`
-	Squad               SquadSettings `json:"squad"`
+	FoodSpawnChance     int            `json:"foodSpawnChance"`
+	MinimumFood         int            `json:"minimumFood"`
+	HazardDamagePerTurn int            `json:"hazardDamagePerTurn"`
+	HazardMap           string         `json:"hazardMap"`
+	HazardMapAuthor     string         `json:"hazardMapAuthor"`
+	Royale              RoyaleSettings `json:"royale"`
+	Squad               SquadSettings  `json:"squad"`
 }
 
-type RoyalSettings struct {
+// RulesetSettings describes all settings specific to a Royale game
+type RoyaleSettings struct {
 	ShrinkEveryNTurns int `json:"shrinkEveryNTurns"`
 }
 
+// SquadSettings describes all settings specific to a Squad game
 type SquadSettings struct {
 	AllowBodyCollisions bool `json:"allowBodyCollisions"`
 	SharedElimination   bool `json:"sharedElimination"`
@@ -85,6 +104,7 @@ type SquadSettings struct {
 	SharedLength        bool `json:"sharedLength"`
 }
 
+// InfoResponse describes the body of the response to 'info' requests.
 type InfoResponse struct {
 	APIVersion string `json:"apiversion"`
 	Author     string `json:"author"`
@@ -94,12 +114,14 @@ type InfoResponse struct {
 	Version    string `json:"version"`
 }
 
+// InfoResponse describes the body of the response to 'move' requests.
 type MoveResponse struct {
 	Move  string `json:"move"`
 	Shout string `json:"shout"`
 }
 
-type battlesnakeServer struct {
+// BattlesnakeServer represents a battlesnake server instance.
+type BattlesnakeServer struct {
 	http.Handler
 	port     string
 	info     *InfoResponse
@@ -107,9 +129,33 @@ type battlesnakeServer struct {
 	moveFunc func(*GameState, *Logger) MoveResponse
 }
 
-func New(port string, info *InfoResponse, logger *log.Logger, loggerOpts int, moveFunc func(*GameState, *Logger) MoveResponse) *battlesnakeServer {
+// New creates a new instance of the battlesnake server with the specified port, InfoResponse, logger, loggerOpts, and moveFunc.
+// The API version is set to the current version.
+//
+// Arguments:
+// - port: The port number to listen on.
+// - info: A pointer to an InfoResponse struct containing information about the battlesnake.
+// - logger: A pointer to a log.Logger instance for to be used for logging.
+// - loggerOpts: An integer representing the logging level options. Use bitwise or `|` to combine options. e.g. `LWarn|LErr|LDebug`.
+// - moveFunc: A function that takes a pointer to a GameState and a pointer to a Logger, and returns a MoveResponse.
+//
+// Returns:
+// - A pointer to a BattlesnakeServer instance.
+//
+// Example usage:
+//
+//	info := &server.InfoResponse{
+//	    APIVersion: "1",
+//	    Author:     "Your Battlesnake Author Name",
+//	    Color:      "#888888",
+//	    Head:       "default",
+//	    Tail:       "default",
+//	    Version:    "0.0.1",
+//	}
+//	server := server.New("8080", info, log.New(os.Stdout, "", 0), 0, moveFunc)
+func New(port string, info *InfoResponse, logger *log.Logger, loggerOpts int, moveFunc func(*GameState, *Logger) MoveResponse) *BattlesnakeServer {
 	info.APIVersion = apiVersion
-	s := &battlesnakeServer{
+	s := &BattlesnakeServer{
 		port:     port,
 		info:     info,
 		logger:   NewLogger(logger, loggerOpts),
@@ -124,7 +170,7 @@ func New(port string, info *InfoResponse, logger *log.Logger, loggerOpts int, mo
 	return s
 }
 
-func (s *battlesnakeServer) Start() error {
+func (s *BattlesnakeServer) Start() error {
 	ln, err := net.Listen("tcp", ":"+s.port)
 	if err != nil {
 		return err
@@ -134,7 +180,7 @@ func (s *battlesnakeServer) Start() error {
 	return http.Serve(ln, s)
 }
 
-func (s *battlesnakeServer) indexHandler() http.HandlerFunc {
+func (s *BattlesnakeServer) indexHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			w.WriteHeader(http.StatusNotFound)
@@ -149,7 +195,7 @@ func (s *battlesnakeServer) indexHandler() http.HandlerFunc {
 	}
 }
 
-func (s *battlesnakeServer) startHandler() http.HandlerFunc {
+func (s *BattlesnakeServer) startHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		state := &GameState{}
 		if err := json.NewDecoder(r.Body).Decode(&state); err != nil {
@@ -161,7 +207,7 @@ func (s *battlesnakeServer) startHandler() http.HandlerFunc {
 	}
 }
 
-func (s *battlesnakeServer) endHandler() http.HandlerFunc {
+func (s *BattlesnakeServer) endHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		state := &GameState{}
 		if err := json.NewDecoder(r.Body).Decode(&state); err != nil {
@@ -173,7 +219,7 @@ func (s *battlesnakeServer) endHandler() http.HandlerFunc {
 	}
 }
 
-func (s *battlesnakeServer) moveHandler() http.HandlerFunc {
+func (s *BattlesnakeServer) moveHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		state := &GameState{}
 		if err := json.NewDecoder(r.Body).Decode(&state); err != nil {
@@ -194,7 +240,7 @@ func (s *battlesnakeServer) moveHandler() http.HandlerFunc {
 	}
 }
 
-func (s *battlesnakeServer) withRequestLogging(next http.HandlerFunc) http.HandlerFunc {
+func (s *BattlesnakeServer) withRequestLogging(next http.HandlerFunc) http.HandlerFunc {
 	if s.logger.Enabled(LDebug) {
 		return func(w http.ResponseWriter, r *http.Request) {
 			b, err := io.ReadAll(r.Body)
